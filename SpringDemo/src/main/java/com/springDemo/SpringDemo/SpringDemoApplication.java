d26f5fea626995eac601fb899228fd9b613f6cb9
lakshmi <sungalarukesh.777@gmail.com>
2024-07-15 13:12:25 +0530

    initial

--- SpringDemo/src/main/java/com/springDemo/SpringDemo/SpringDemoApplication.java
+++ SpringDemo/src/main/java/com/springDemo/SpringDemo/SpringDemoApplication.java
@@ -2,13 +2,21 @@ package com.springDemo.SpringDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoApplication.class, args);
-		System.out.println("Hello worldgit ");
+		ApplicationContext context = SpringApplication.run(SpringDemoApplication.class, args);
+
+		/*Normal Object
+		Alien obj = new Alien();
+		 */
+
+		Alien obj = context.getBean(Alien.class);
+		obj.code();
+
 	}
 
 }
