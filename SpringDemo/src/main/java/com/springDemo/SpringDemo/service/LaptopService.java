0851016bc94443b5935863c8124dc7f6a6cb1d39
lakshmi <sungalarukesh.777@gmail.com>
2024-07-29 08:25:21 +0530

    initial

--- SpringDemo/src/main/java/com/springDemo/SpringDemo/service/LaptopService.java
+++ SpringDemo/src/main/java/com/springDemo/SpringDemo/service/LaptopService.java
@@ -1,16 +1,14 @@
 package com.springDemo.SpringDemo.service;
 
-import com.springDemo.SpringDemo.LaptopRepository;
+import com.springDemo.SpringDemo.repo.LaptopRepository;
 import com.springDemo.SpringDemo.model.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LaptopService {
-
     @Autowired
     private LaptopRepository repo;
-
     public void addLaptop(Laptop lap){
         repo.save(lap);
     }
